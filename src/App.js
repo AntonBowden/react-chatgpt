e06d3ac3129fa40be0ffed05f133e95c919686eb
Anton Bowden <bowden.anton@gmail.com>
2024-02-23 10:35:30 +0000

    add styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ const App = () => {
   return (
     <div className="app">
       <section className="sidebar">
-        <button>+ New Chat</button>
+        <button>New Chat</button>
+        <ul className="history">
+          <li>Blah</li>
+        </ul>
+        <nav>
+          <p>Made by Anton</p>
+        </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>AntonGPT</h1>
+        <ul className="feed"></ul>
+
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">Send</div>
+          </div>
+          <p className="info">
+            ChatGPT can make mistakes. Consider checking important information.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
